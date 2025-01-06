8fa9f220a203ff8e3ce3e20f75a66095a5055428
kate <kate@dariox.club>
2025-01-06 19:13:17 +0800

    [index] Create dist directory when it doesn't exist

--- index.ts
+++ index.ts
@@ -6,6 +6,22 @@ import * as _credits from './credits.js'
 
 import * as generate from './src/generate.ts'
 
+const exists = async (filename: string): Promise<boolean> => {
+    try {
+        await Deno.stat(filename);
+        // successful, file or directory must exist
+        return true;
+    } catch (error) {
+        if (error instanceof Deno.errors.NotFound) {
+            // file or directory does not exist
+            return false;
+        } else {
+            // unexpected error, maybe permissions, pass it along
+            throw error;
+        }
+    }
+};
+
 let decoder = new TextDecoder('utf-8')
 export const config: IConfig = {
     imagePath: path.resolve('./img/'),
@@ -20,6 +36,10 @@ console.log(`Files in ${path.resolve('./')}`)
 for (let item of Deno.readDirSync(path.resolve('./')))
 {
     console.log(`- ${item.name}` + (item.isDirectory ? ' (directory)' : ''))
+}
+if (!exists(config.distPath))
+{
+    Deno.mkdirSync(config.distPath);
 }
 console.log(`Reading all files in ${config.distPath}`);
 for (let item of Deno.readDirSync(config.distPath))
